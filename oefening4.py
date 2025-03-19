3cfa3a24aad6826c7152957269ab74e33421b4fb
tyreece omitola <omitolatyreece@gmail.com>
2025-03-19 17:07:26 +0100

    30min

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,43 @@
-def countTargetPairs():
+import random
+
+def randnumgen():
+    startinput = int(input("start " ))
+    endinput = int(input("end " ))
+    leninput = int(input("length " ))
+    num_list = []
+
+    while len(num_list) < leninput:
+        num_list.append(random.randint(startinput, endinput))
+    return num_list
+
+
+nums =  randnumgen()
+
+
+def countTargetPairs(nums):
+    target = int(input("input target "))
+    TrPairs = 0
+    for i in range(len(nums)):
+        for j in range( i + 1, len(nums)):
+            if i != j:
+                if nums[i] + nums[j] < target:
+                    TrPairs += 1
+    return TrPairs
+
+
+print(countTargetPairs(nums))
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
